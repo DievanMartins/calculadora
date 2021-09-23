6a4976b38e90f881b888b2edff819878ed00b199
DievanMartins <85848490+DievanMartins@users.noreply.github.com>
2021-09-23 12:18:38 -0300

    Add files via upload

---
+++ calculadora/app.js
@@ -0,0 +1,29 @@
+
+//inserir os números e sinais no display
+function insert(num){
+  let amount = document.querySelector('#result').innerHTML;
+  document.querySelector('#result').innerHTML = amount + num;
+}
+
+//limpar display
+function clean(){
+  document.querySelector('#result').innerHTML = '';
+}
+
+//limpa apenas um item de cada vez do display
+function cleanOne(){
+  let res = document.querySelector('#result').innerHTML;
+  document.querySelector('#result').innerHTML = res.substring(0, res.length - 1);
+}
+
+//calcular
+//o eval é responsável por fazer os calculos
+function calculate(){
+  let res = document.querySelector('#result').innerHTML;
+  if (res) {
+    document.querySelector('#result').innerHTML = eval(res);
+    console.log(eval(res))
+  } else {
+    document.querySelector('#result').innerHTML = 'ERRO';
+  }
+}
